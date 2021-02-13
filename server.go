ef79759e87ccdb680e9f55c18f2c0b0a153d63e0
Igor Botukhov <ibotukhov@gmail.com>
2021-02-13 13:54:28 +0300

    All is work!!!

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var templates = template.Must(template.ParseFiles("page.html"))
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	err := templates.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", mainPage)
+	log.Println("Server start at port :8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
