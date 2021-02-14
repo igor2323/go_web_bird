3aac33005354e7ba04ee83daa08f6016521289cd
Igor Botukhov <ibotukhov@gmail.com>
2021-02-14 19:11:23 +0300

    adaptive site!

--- server.go
+++ server.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
-var templates = template.Must(template.ParseFiles("page.html"))
+var main_temp = template.Must(template.ParseFiles("main_page.html"))
+var game_temp = template.Must(template.ParseFiles("game_page.html"))
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-	err := templates.Execute(w, nil)
+	err := main_temp.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func birdGame(w http.ResponseWriter, r *http.Request) {
+	err := game_temp.Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func main() {
+	server_port := flag.Int("port", 8000, "The port on which the server is running")
+	flag.Parse()
+
 	http.HandleFunc("/", mainPage)
-	log.Println("Server start at port :8002")
-	log.Fatal(http.ListenAndServe(":8002", nil))
+	http.HandleFunc("/bird_game", birdGame)
+	log.Println("Server start at port " + strconv.Itoa(*server_port))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*server_port), nil))
 }
